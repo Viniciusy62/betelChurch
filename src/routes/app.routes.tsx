89674c1346c55d770033fa35125332c2e11a11be
Vinicius Andrade <viniciusy62@gmail.com>
2024-12-28 18:37:55 -0300

    delete components

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
-import { Donations } from "../pages/Donations";
 
 export function AppRoutes() {
   return(
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/donations" element={<Donations/>} />
     </Routes>
   )
 }
