6e1dc0dc0f52c16f6224f6ba4a6b64fba0e80988
Vinicius Andrade <viniciusy62@gmail.com>
2024-12-28 20:25:33 -0300

    Create home page with header, main, presentation section and worship time section and style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,51 @@
 import { Container } from "./styles";
+import Logo from "../../public/logo-cruz-betel.png";
+import { FiMenu } from "react-icons/fi"
+import { Button } from "../../components/button";
+
 export function Home() {
   return (
     <Container>
-      
+      <header>
+        <img src={Logo} alt="Logo da Igreja Betel" />
+
+        <FiMenu size={24} color="#fff" />
+      </header>
+
+      <main>
+        <p>Uma igreja que ama Jesus Cristo, que ama fazer discípulos e que ama pessoas</p>
+
+        <h1>Igreja Betel</h1>
+
+        <Button title="Venha participar conosco"/>
+
+        <Button title="Confira nosso calendário" />
+      </main>
+
+      <section className="apresentation">
+        <p>A Igreja Betel tem o propósito de estabelecer o Reino de Deus na terra  através da difusão do Evangelho de Jesus Cristo a todos os povos,  línguas e nações dentro de uma visão missionária</p>
+        <p>“A colheita é grande, mas os trabalhadores são  poucos. Portanto, peçam ao Senhor da colheita que mande trabalhadores  para a sua colheita. Lc 10.2</p>
+        <p>Somos uma família que cresce a cada dia com a missão de levar cada pessoa a ser um discípulo de Jesus Cristo em nossa comunidade local e afins. Servir em amor é o nosso  propósito de vida!</p>
+      </section>
+
+      <section className="whorship-time">
+        <h2>CULTOS</h2>
+
+        {/* <section className="images-worship-time">
+          <img src={Logo} alt="" />
+          <img src={Logo} alt="" />
+
+          <h3>Há lugar para você no que Deus está fazendo em nosso meio</h3>
+        </section> */}
+
+        <p>
+          <span>Venha participar conosco</span>
+          <span>Quartas</span>
+          <span>19h30</span>
+          <span>Domingos</span>
+          <span>18h</span>
+        </p>
+      </section>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,100 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
- 
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 1.6rem;
+
+    background-color: #111111;
+    width: 100%;
+
+    img {
+      width: clamp(3rem, 4rem, 6rem);
+    }
+  }
+
+  main {
+    padding: 1.6rem;
+    background: #333;
+    height: 100vh;
+
+    p,
+    h1 {
+      color: #fff;
+    }
+
+    p {
+      font-size: 1.6rem;
+    }
+
+    h1 {
+      font-size: 4.2rem;
+      font-weight: 400;
+      margin: 1.6rem auto 0.8rem;
+    }
+
+    button {
+      margin-top: 1.6rem;
+      width: 21rem;
+    }
+  }
+
+  .apresentation {
+    padding: 1.6rem;
+
+    p:nth-child(1) {
+      font-family: "Lora", serif;
+      font-weight: 400;
+      font-size: 2.4rem;
+    }
+
+    p:nth-child(2) {
+      font-style: italic;
+      font-weight: 100;
+    }
+
+    p {
+      margin-bottom: 2.4rem;
+      font-size: 1.8rem;
+    }
+  }
+
+  .whorship-time {
+    padding: 1.6rem;
+
+    h2 {
+      margin-bottom: 4.8rem;
+      font-family: "Lora", serif;
+      font-size: 3.2rem;
+      font-weight: 400;
+    }
+
+    .images-worship-time {
+      display: flex;
+      flex-wrap: nowrap;
+    }
+
+    p {
+      font-size: 1.6rem;
+      font-weight: 600;
+
+      display: flex;
+      flex-direction: column;
+      gap: 0.4rem;
+      align-items: center;
+      
+      span:nth-child(1) {
+        font-size: 2.4rem;
+      }
+
+      span:nth-child(even) {
+        font-size: 1.8rem;
+        font-weight: bold;
+      }
+    }
+
+  }
 `;
