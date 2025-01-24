286394353017aca57c3b8e9832c4574d51d6059b
Vinicius Andrade <viniciusy62@gmail.com>
2025-01-24 17:20:46 -0300

    update header component with break point resize

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
+import Logo from "../../public/logo/logo-cruz-betel.png";
 import { Container } from "./styles";
-import Logo from "../../public/logo-cruz-betel.png";
 import { MobileMenu } from "../../components/mobileMenu";
+import { Menu } from "../menu";
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakPoints';
 
 export function Header() {
-  
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= DEVICE_BREAKPOINTS.MD);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= DEVICE_BREAKPOINTS.MD);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Container>
       <img src={Logo} alt="Logo da Igreja Betel" />
-      <MobileMenu />
+
+      {isMobile ? <MobileMenu/> : <Menu/>}
     </Container>
   );
 }
